5e53a288771e0967b298e4d12befee5e4dfb7896
Santiago Veloso <96207233+santyveloso@users.noreply.github.com>
2025-07-14 04:23:49 +0100

    fix: hidden vars

--- firebase.js
+++ firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBp55c0-J231N7pMpUT3oMAkMMSvh7Bsko",
-    authDomain: "promptloom-b49be.firebaseapp.com",
-    projectId: "promptloom-b49be",
-    storageBucket: "promptloom-b49be.firebasestorage.app",
-    messagingSenderId: "242229225365",
-    appId: "1:242229225365:web:f2a4d91b075057dda7389c",
-    measurementId: "G-C8EC00RM22"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig)
